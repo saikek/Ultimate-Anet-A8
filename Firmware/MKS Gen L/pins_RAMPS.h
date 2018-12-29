ebb4dd6ea62f274ecc550998f280062d7ce0fd69
saikek <saikek@mail.ru>
2018-12-29 09:28:37 +0100

    Pins update

--- Firmware/MKS Gen L/pins_RAMPS.h
+++ Firmware/MKS Gen L/pins_RAMPS.h
@@ -582,3 +582,25 @@
   #endif // NEWPANEL
 
 #endif // ULTRA_LCD
+      64
+        #define BTN_EN2         59
+        #define BTN_ENC         63
+      #elif ENABLED(PANEL_ONE)
+        #define BTN_EN1         59   // AUX2 PIN 3
+        #define BTN_EN2         63   // AUX2 PIN 4
+        #define BTN_ENC         49   // AUX3 PIN 7
+      #else
+        #define BTN_EN1         37
+        #define BTN_EN2         35
+        #define BTN_ENC         31
+      #endif
+
+      #if ENABLED(G3D_PANEL)
+        #define SD_DETECT_PIN   49
+        #define KILL_PIN        41
+      #endif
+
+    #endif
+  #endif // NEWPANEL
+
+#endif // ULTRA_LCD
